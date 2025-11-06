0fbdacee88c2de7b208936fce3676712fae6202b
TeAnLi <m1055965862@163.com>
2025-11-06 11:01:10 +0800

    feat: 添加基础的QQ群聊插件

---
+++ plugins/group/plugin.py
@@ -0,0 +1,10 @@
+from ncatbot.plugin_system import NcatBotPlugin
+from ncatbot.plugin_system import NcatBotPlugin
+
+class GroupPlugin(NcatBotPlugin):
+    name = "GroupPlugin"
+    version = "0.0.1"
+    author = "TeAnli"
+    description = "QQ群聊基础插件"
+
+    
