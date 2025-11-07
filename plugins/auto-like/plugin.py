614e21925f8abb0b65b766cd9708daeac330e4cd
TeAnLi <m1055965862@163.com>
2025-11-07 16:08:02 +0800

    fix: 为自动点赞绕过QQ风控

--- plugins/auto-like/plugin.py
+++ plugins/auto-like/plugin.py
@@ -1,19 +1,18 @@
 from ncatbot.plugin_system import NcatBotPlugin
 from ncatbot.plugin_system import command_registry
-
 from ncatbot.core.event import BaseMessageEvent
-
 from ncatbot.utils import get_log
 
-from enum import Enum
-
+import random
 _logger = get_log()
 
-class LikeCount(Enum):
-    DEFAULT_LIKE_COUNT = 10
-    SVIP_LIKE_COUNT = 20
-    MAX_LIKE_COUNT = 50
-
+message_list = [
+    "赞过了哦",
+    "赞了赞了",
+    "赞过了",
+    "点赞成功",
+    "已点赞",
+]
 
 class AutoLikePlugin(NcatBotPlugin):
     name = "AutoLikePlugin"
@@ -24,20 +23,21 @@ class AutoLikePlugin(NcatBotPlugin):
     @command_registry.command("点赞", description="对你的名片进行点赞")
     async def like_command(self, event: BaseMessageEvent):
         status = await self.api.get_status()
-        _logger.info(f"User {event.user_id} liked their profile. Status: {status}")
+        _logger.info(f"User {event.user_id} requested like. Status: {status}")
+        count = 0
+        try:
+            for _ in range(5):
+                await self.api.send_like(event.user_id, times=10)
+                count += 10
+                _logger.info(f"Sent 10 likes in batch, total count: {count}")
+        except Exception as e:
+            _logger.warning(f"Like action encountered an error after {count} likes: {e}")
 
-        for i in range(0, 50):
-            response = await self.api.send_like(event.user_id)
-            if response["retcode"] == 1200 and response["status"] == "failed":
-                _logger.info(f"Failed to like profile {i + 1} times. because: {response['msg']}")
-                await self.api.send_group_msg(event.group_id, [ 
-                    { 
-                        "type": "text", 
-                        "data": { 
-                            "text" : f"点赞失败，原因：点赞次数达到最大上限哦"
-                        } 
-                    },
-                ])
-                break
-            else:
-                _logger.info(f"Successfully liked profile {i + 1} times.")
+        await self.api.send_group_msg(event.group_id, [
+            {
+                "type": "text",
+                "data": {
+                    "text": f"{random.choice(message_list)}"
+                }
+            }
+        ])
